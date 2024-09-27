bb75100462221bdbcd0c8c27cf49bac8d038d259
Tiger-Foxx <donfackarthur750@gmail.com>
2024-09-27 17:23:39 +0100

    branche la plus a jour

--- asset/js/utils/listeners.js
+++ asset/js/utils/listeners.js
@@ -11,4 +11,15 @@ $('#educationalPoints').on('click', function(){
       layerGroup.addLayer(educationalPoints);
     }
   })
+
+  // PREVUS POUR D'AUTRES OPTIONS 
+
+  $('#autresPoints').on('click', function(){
+    $(this).toggleClass("open");
+    // if(layerGroup.hasLayer(educationalPoints)){
+    //   layerGroup.removeLayer(educationalPoints);
+    // }else{
+    //   layerGroup.addLayer(educationalPoints);
+    // }
+  })
   
