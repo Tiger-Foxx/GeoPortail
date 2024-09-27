95238f9e73e0af5b2ea094948321649d79cbd919
Tiger-Foxx <donfackarthur750@gmail.com>
2024-09-27 18:01:17 +0100

    branche la plus a jour

--- asset/js/utils/dessin.js
+++ asset/js/utils/dessin.js
@@ -21,7 +21,7 @@ PARAMETRES : GeoPointsDatas (represente le set de points charges depuis le GeoJs
  * @param {L.Map} TheMap - La carte Leaflet
  * @returns {Object} - Un objet contenant le nombre de points, la superficie, le périmètre et le type de forme dessinée
  */
-function CountEntytiesInZone(GeoPointsDatas, drawnItems, label, TheMap) {
+function CountEntytiesInZoneOld(GeoPointsDatas, drawnItems, label, TheMap) {
   var result = {
       pointsInBounds: 0,
       area: 0,
@@ -149,4 +149,77 @@ function AddPoints(pointsGoJson, TheMap,color='#3388ff',fillColor='#3388ff') {
   }).addTo(TheMap);
 
   return Points;
+}
+
+
+/** CE FICHIER REGROUPE DES FONCTIONS UTILITAIRES POUR LE DESSIN , IL FAUT TOUJOURS LE CHARGER AVANT LE MAP.JS * */
+
+/**
+ * Cette fonction compte le nombre d'entités dans la zone de dessin et renvoie un objet avec des informations supplémentaires.
+ * Elle attache également des événements pour afficher un popup sur le survol de la zone dessinée.
+ * 
+ * @param {L.GeoJSON} GeoPointsDatas - Le set de points GeoJSON chargé sur la carte
+ * @param {L.FeatureGroup} drawnItems - Le groupe de points qui ont été dessinés
+ * @param {string} label - Le label pour afficher dans l'alerte
+ * @param {L.Map} TheMap - La carte Leaflet
+ * @returns {Object} - Un objet contenant le nombre de points, la superficie, le périmètre et le type de forme dessinée
+ */
+function CountEntytiesInZone(GeoPointsDatas, drawnItems, label, TheMap) {
+  var result = {
+      pointsInBounds: 0,
+      area: 0,
+      perimeter: 0,
+      shapeType: ''
+  };
+  var big = false;
+
+  TheMap.on(L.Draw.Event.CREATED, function (event) {
+      var layer = event.layer;
+      drawnItems.addLayer(layer);
+
+      var bounds = layer.getBounds();
+      result.pointsInBounds = 0;
+      
+      GeoPointsDatas.eachLayer(function (pointLayer) {
+          if (bounds.contains(pointLayer.getLatLng())) {
+              result.pointsInBounds++;
+          }
+      });
+
+      if (layer instanceof L.Polygon || layer instanceof L.Rectangle) {
+          var latlngs = layer.getLatLngs()[0];
+          result.area = L.GeometryUtil.geodesicArea(latlngs);
+          result.perimeter = L.GeometryUtil.length(latlngs);
+          result.shapeType = layer instanceof L.Rectangle ? 'Rectangle' : 'Polygon';
+      } else if (layer instanceof L.Circle) {
+          result.area = Math.PI * Math.pow(layer.getRadius(), 2);
+          result.perimeter = 2 * Math.PI * layer.getRadius();
+          result.shapeType = 'Circle';
+      }
+
+      if (result.area > 1000000 / 2) {
+          big = true;
+          result.area = result.area / 1000000;
+      }
+      var unit = !big ? 'm²' : 'km²';
+
+      // Créer un popup sur le survol de la forme
+      layer.on('mouseover', function () {
+          var popupContent = `
+              <strong>${label} dans cette zone :</strong><br>
+              Nombre de ${label} : ${result.pointsInBounds}<br>
+              Superficie : ${result.area.toFixed(2)} ${unit}<br>
+              Périmètre : ${result.perimeter.toFixed(2)} m<br>
+              Type de forme : ${result.shapeType}
+          `;
+          layer.bindPopup(popupContent).openPopup();
+      });
+
+      // Supprimer le popup lorsqu'on quitte la forme
+      layer.on('mouseout', function () {
+          layer.closePopup();
+      });
+  });
+
+  return result;
 }
