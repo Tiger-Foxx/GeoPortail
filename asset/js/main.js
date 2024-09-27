77e3c4aa22087419c4f5a8878d72df43709848ce
YarlOverdose <55983965+YarlOverdose@users.noreply.github.com>
2024-09-27 15:13:55 +0100

    Add files via upload

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,21 +6,52 @@ $(".search-block input").focusout(function(){
     $(".search-container").removeClass("focus");
 })
 
-$(".btn-menu").on("click", function(e){
+$(".js-btn-menu").on("click", function(e){
     let on = $(".on");
     
     if($(this)[0] != on[0]){
         on.removeClass("on");
         on.find(".modal").hide();
     }
-    
     $(this).find(".modal").toggle();
     $(this).toggleClass("on");
+})
+$(document).on('click', function(e){
+    if(!$(this).hasClass('rel')){
+        console.log('ok')
+        let on = $(".out");
+        on.find(".c-menu").hide();
+        on.removeClass("out");
+        on.removeClass("active");
+    }
+})
+$(".js-btn-c-menu").on("click", function(e){
+    let on = $(".out");
+    
+    if($(this)[0] != on[0]){
+        on.removeClass("out");
+        on.find(".c-menu").hide();
+    }
+    
+    $(this).find(".c-menu").toggle();
+    $(this).toggleClass("out");
 })
 $(".modal").on("click", function(e){
     e.stopPropagation();
 })
 
 $('.p-obj').on('click', function(e){
+    $(this).toggleClass('active');
+})
+
+$('.btn-modal').on('click', function(e){
+    let active = $(".active");
+    if($(this)[0] != active [0]){
+        active.removeClass("active");
+        let tg = active.attr('element');
+        $('#' + tg).hide();
+    }
+    let target = $(this).attr('element');
+    $('#' + target).toggle();
     $(this).toggleClass('active');
 })
--- asset/js/map.js
+++ asset/js/map.js
@@ -21,28 +21,27 @@ function init() {
   );
 
   mainLayer.addTo(map);
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* INITIALISATION DE LA CARTE */
-
 init();
 
 /* AJOUT DU SET DE POINTS GEOJSON A LA CARTE */
+var educationalPoints = AddPoints(EDUCATIONAL_POINTS,TheMap=map);
 
-var educationalPoints=AddPoints(EDUCATIONAL_POINTS,TheMap=map);
+/* CREATION DU LAYERGROUP */
+
+var layerGroup = L.layerGroup([educationalPoints]);
+layerGroup.addTo(map);    // Adding layer group to map
+
+$('#educationalPoints').on('click', function(){
+  $(this).toggleClass("open");
+  if(layerGroup.hasLayer(educationalPoints)){
+    layerGroup.removeLayer(educationalPoints);
+  }else{
+    layerGroup.addLayer(educationalPoints);
+  }
+})
 
 // ########### OPTIONS DE DESSIN ############################ //
 
