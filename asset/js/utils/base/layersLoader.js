16c61670d88c900968894b1732606de9abd2e6e8
Tiger-Foxx <donfackarthur750@gmail.com>
2024-10-10 15:29:17 +0100

    resolution des conflits apres organisatio du code

--- asset/js/utils/base/layersLoader.js
+++ asset/js/utils/base/layersLoader.js
@@ -5,13 +5,7 @@ UNE FOIS LES DATAS CHARGES ON AJOUTE LES OPTIONS DE DESSIN ET AUTRES
 
 */
 
-/** INITIALISATION DES CONTANTES */
 
-const EDUCATIONAL_POINTS = PointsEducatifs;
-const HEATH_CENTERS=hopitaux;
-const WATER_POINTS=CourtEau;
-
-/** INITIALISATION DES CONTANTES */
 
 async function getRandomColor() {
     const letters = '0123456789ABCDEF';
@@ -28,28 +22,6 @@ var layerGroup = L.layerGroup([]);
 var layerBoundsGroup=L.layerGroup([]); // un layergroup qui est different du precedent par ils ne s'agiut pas de points mais d'unite administratives
 var layerList=[]   //UNE LISTE DE CHAINES QUI DONNES LES LABELS DES DIFFEENTS LAYERS
 
-
-
-
-
-/* CENTRE */
-var departements=null;
-var arrondissements=null;
-var communes=null;
-
-/* CENTRE */
-
-
-
-
-/* EDUCATION */
-
-var superieur=null;
-var secondaire=null;
-var primaire= null;
-
-
-/* EDUCATION */
 
 
 
@@ -93,31 +65,37 @@ function dropFromLayerList(chaine) {
 
 
 
-
+var departements={
+  nom:"Departements",
+  htmlID:"#departements",
+  layerName:"centre:departements",
+  url:"http://srv558546.hstgr.cloud:8080/geoserver/centre/wms",
+  layer:null,
+};
 
 initCenter =function () {
-  if (departements != null && layerBoundsGroup.hasLayer(departements)) {
-    layerBoundsGroup.removeLayer(departements);
+  if (departements.layer != null && layerBoundsGroup.hasLayer(departements.layer)) {
+    layerBoundsGroup.removeLayer(departements.layer);
   } else {
-    if (departements == null) {
+    if (departements.layer == null) {
       $('#departements').toggleClass("open");
       (async function () {
         var color = await getRandomColor();
-        departements = await AddPointsWFS({
+        departements.layer = await AddPointsWFS({
           fromGeoServer: true,
           TheMap: map,
-          layer: "centre:departements",
-          url: "http://srv558546.hstgr.cloud:8080/geoserver/centre/wms",
+          layer: departements.layerName,
+          url: departements.url,
           opacity: 0.5,
           fillColor: color,
           color: color,
         });
 
         // Ajouter le layer au layerBoundsGroup
-        layerBoundsGroup.addLayer(departements);
+        layerBoundsGroup.addLayer(departements.layer);
       })();
     } else {
-      layerBoundsGroup.addLayer(departements);
+      layerBoundsGroup.addLayer(departements.layer);
     }
   }
 }()
@@ -158,11 +136,16 @@ initCenter =function () {
 
 
 
-// ########### OPTIONS DE DESSIN ############################ //
+/* DESSIN */
 
 var drawnItems= InitDraw(TheMap=map);
 NewCountEntytiesInZone(GeoPointsDatasList=layerGroup.getLayers() , drawnItems=drawnItems , labels=layerList,TheMap=map)
 
-// Initialiser la barre de recherche pour la recherche de pointsd chargees dans le layergroup  juste
+/* DESSIN */
+
+
+/* RECHERCHE */
 
 setupSearch(geoJsonLayers=layerGroup.getLayers(), TheMap=map);
+
+/* RECHERCHE */
